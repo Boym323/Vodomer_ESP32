c35401d9a97ed95426422de0a07f9aa83d44ef1e
Boym323 <martin@pomykal.cz>
2020-06-12 16:05:44 +0200

    Doplněna Wifi
    plus pokus s MQTT

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,23 @@
-#include <Arduino.h>
+//#include <Arduino.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+const char *ssid = "WiFi_name";
+const char *password = "password";
+const char *mqtt_server = "192.168.0.2";
 
 #define PIN_napajeni_optosenzoru 12
 #define PIN_SV 27
 
-RTC_DATA_ATTR size_t litry = 0;
+RTC_DATA_ATTR size_t PocetProbuzeni = 0;
+
+RTC_DATA_ATTR size_t SpotrebaSV = 0;
 RTC_DATA_ATTR size_t StavSV;
 RTC_DATA_ATTR size_t posledniStavSV;
 
 void setup()
 {
- Serial.begin(115200);
+  Serial.begin(115200);
   pinMode(PIN_napajeni_optosenzoru, OUTPUT);
   pinMode(PIN_SV, INPUT_PULLUP);
 }
@@ -28,16 +36,16 @@ void OdecetVody()
     {
       // jestliže je současný stav HIGH, tlačítko přešlo
       //z off na on:
-      litry++;
+      SpotrebaSV++;
       Serial.println("impuls");
       Serial.print("Pocet litrů od startu:  ");
-      Serial.println(litry);
+      Serial.println(SpotrebaSV);
     }
     else
     {
       // jestliže je současný stav LOW, tlačítko přešlo
       // z on na off:
-      Serial.println("off");
+      Serial.println("Není ");
     }
   }
   // uložte současný stav jako „poslední stav“,
@@ -46,11 +54,44 @@ void OdecetVody()
   digitalWrite(PIN_napajeni_optosenzoru, LOW);
 }
 
+void wifi()
+{
+  Serial.begin(115200);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password); //  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  Serial.print("RSSI: ");
+  Serial.println(WiFi.RSSI());
+  WiFiClient espClient;
+  PubSubClient client(mqtt_server, 1883, espClient);
+  client.publish("domov/temperature1", String(30).c_str(), true);
+  client.publish("outTopic", "hello world");
+}
+
 void loop()
 {
-OdecetVody();
-Serial.println(millis());
-esp_sleep_enable_timer_wakeup(1000000);
-esp_deep_sleep_start();
+  PocetProbuzeni++;
+  OdecetVody();
+  Serial.print(PocetProbuzeni);
+  if (PocetProbuzeni >= 10 && SpotrebaSV > 0)
+  {
+    wifi();
+    PocetProbuzeni = 0;
+    SpotrebaSV = 0;
+  }
 
+  esp_sleep_enable_timer_wakeup(1000000);
+  esp_deep_sleep_start();
 }
